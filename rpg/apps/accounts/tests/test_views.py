2b6fe389fa27eecf237bf2e45b2ad8a8b73b72d1
jeffsolo <jeffreycsolomon@gmail.com>
2019-03-21 20:17:21 +0100

    expanded tests

--- rpg/apps/accounts/tests/test_views.py
+++ rpg/apps/accounts/tests/test_views.py
@@ -3,16 +3,31 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class TestMainViews(TestCase):
-    def test_homepage_status_code(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-
-
-class TestAccountsViews(TestCase):
+class ClientTestCase(TestCase):
     @classmethod
     def setUp(cls):
         cls.client = Client()
+
+    def check_status_code_200(self, viewname):
+        url = reverse(viewname)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def check_csrf(self, viewname):
+        response = self.client.get(reverse(viewname))
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+
+class TestMainViews(ClientTestCase):
+    def test_homepage_status_code(self):
+        self.check_status_code_200('home')
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+
+class TestAccountsViews(ClientTestCase):
+    @classmethod
+    def setUp(cls):
         cls.credentials = {
             'username': 'test_user',
             'password1': 'test_password1',
@@ -20,17 +35,17 @@ class TestAccountsViews(TestCase):
         }
 
     def test_signup_status_code(self):
-        url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.check_status_code_200('signup')
 
     def test_signup_redirect(self):
         response = self.client.post(reverse('signup'), self.credentials)
         self.assertRedirects(response, reverse('home'))
 
     def test_signup_csrf(self):
-        response = self.client.get(reverse('signup'))
-        self.assertContains(response, 'csrfmiddlewaretoken')
+        self.check_csrf('signup')
 
+    def test_login_status_code(self):
+        self.check_status_code_200('login')
 
-
+    def test_login_csrf(self):
+        self.check_csrf('login')
