216bc8ee78c2249ff7323adfae00e794dab44282
JeffSolo <jeffreycsolomon@gmail.com>
2020-03-22 17:15:45 +0100

    basic profile view

--- rpg/rpg/views.py
+++ rpg/rpg/views.py
@@ -1,6 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.views import generic
+from apps.creator.models import Characters
 
 
 # TODO remove after figuring out where to actually redirect to after successful login/signup
-class Home(generic.TemplateView):
+class HomeView(generic.TemplateView):
     template_name = 'home.html'
+
+
+class ProfileView(LoginRequiredMixin, generic.ListView):
+    model = Characters
+    template_name = 'rpg/profile.html'
+    paginate_by = 10
+    context_object_name = 'characters'
+
+    def get_queryset(self):
+        return Characters.objects.filter(user=self.request.user).order_by('name')
