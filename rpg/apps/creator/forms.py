b340f04c82ba7f3ab6bb39da5c9be6bc8aef0e6a
JeffSolo <jeffreycsolomon@gmail.com>
2020-04-15 19:48:11 +0200

    add buttons plus ajax request

--- rpg/apps/creator/forms.py
+++ rpg/apps/creator/forms.py
@@ -18,19 +18,20 @@ class LevelModelChoiceField(forms.ModelChoiceField):
 
 
 class CharacterForm(forms.ModelForm):
-    #user = forms.CharField(widget=forms.TextInput(attrs={'readonly':'True'}))
     character_class = ClassModelChoiceField(queryset=Classes.objects.all(), empty_label=None)
     character_race = RaceModelChoiceField(queryset=Races.objects.all(), empty_label=None)
     level = forms.IntegerField(min_value=1, max_value=20, initial=1, disabled=True)
-    strength        = forms.IntegerField(label='STR', initial=10)
-    dexterity       = forms.IntegerField(label='DEX', initial=10)
-    constitution    = forms.IntegerField(label='CON', initial=10)
-    intelligence    = forms.IntegerField(label='INT', initial=10)
-    wisdom          = forms.IntegerField(label='WIS', initial=10)
-    charisma        = forms.IntegerField(label='CHA', initial=10)
+    abil_strength        = forms.IntegerField(label='STR', initial=8)
+    abil_dexterity       = forms.IntegerField(label='DEX', initial=8)
+    abil_constitution    = forms.IntegerField(label='CON', initial=8)
+    abil_intelligence    = forms.IntegerField(label='INT', initial=8)
+    abil_wisdom          = forms.IntegerField(label='WIS', initial=8)
+    abil_charisma        = forms.IntegerField(label='CHA', initial=8)
+    scores = forms.CharField(label='Ability Scores', initial="8, 10, 12, 13, 14, 15")
+
     class Meta:
         model = Characters
         exclude = ['user', 'character_info']
         widgets = {
-            'user': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'scores': forms.TextInput(attrs={'readonly': 'True'})
         }
--- rpg/apps/dice/common.py
+++ rpg/apps/dice/common.py
@@ -3,7 +3,7 @@ from dice import roll
 
 # TODO: add possible variations: reroll lowest stat, reroll ones, etc
 def roll_stats():
-    return [roll('4d6^3t') for _ in range(6)]
+    return sorted([roll('4d6^3t') for _ in range(6)])
 
 # rN rerolls <= n
 # ^n keeps highest n
